dfcd1b06f36ab4908abe192239df7803c8d88160
Hunegn <demissiehunegna@gmail.com>
2024-12-27 11:19:31 +0300

    EDA

--- src/fileChange.py
+++ src/fileChange.py
@@ -1,9 +1,15 @@
 import pandas as pd
 
-input_file = '../Data/MachineLearningRating_v3.txt'
-output_file = '../Data/MachineLearningRating_v3.csv'
+input_file = '../Data/raw/MachineLearningRating_v3.txt'
+output_file = '../Data/raw/MachineLearningRating_v3.csv'
 
-df = pd.read_csv(input_file, delimiter='\t')  # Change '\t' to ',' or ' ' based on your file
+df = pd.read_csv(input_file, delimiter='|')  # Use '|' as the delimiter
+
+# Save the parsed data to a new .csv file
 df.to_csv(output_file, index=False)
+
+print(f"File successfully parsed and saved as {output_file}")
+print(df.head())
+print(df.info())
 
 print(f"File converted and saved as {output_file}")
